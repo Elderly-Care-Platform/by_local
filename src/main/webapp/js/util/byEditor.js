f9ad2936cc76e0bdaca6631281a7d29e50766956
sanjukta <sanjuktas@beautifulyears.com>
2015-06-17 14:59:17 +0530

    text cut fix

--- src/main/webapp/js/util/byEditor.js
+++ src/main/webapp/js/util/byEditor.js
@@ -1,86 +1,112 @@
 
 var BY = BY || {};
+BY.textAreas = [];
 
-BY.initEditor = function initEditor(param){
-    //tinymce.remove();
-    var tinyEditor = tinymce.get(param.editorTextArea);
-    if(tinyEditor){
-        tinyEditor.remove();
+BY.removeEditor = function(){
+    var textAreas = $("textarea");
+    for (var i = 0; i < textAreas.length; i++) {
+        //Check if element already has editor enabled
+        if (tinymce.get(textAreas[i].id))
+            tinyMCE.execCommand("mceRemoveEditor", false, textAreas[i].id);
     }
 
-    tinymce.init({
-        selector: "#"+param.editorTextArea,
-        theme: "modern",
-        skin: 'light',
-        statusbar: false,
-        menubar: false,
-        plugins: [
-            "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-            "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-            " emoticons textcolor paste autoresize "
-        ],
-        toolbar: "styleselect | bold italic | bullist numlist hr  | undo redo | link unlink emoticons image media  preview ",
-        setup : function(ed) {
-            var placeholder = $('#' + ed.id).attr('placeholder');
-            if (typeof placeholder !== 'undefined' && placeholder !== false) {
-                var is_default = false;
-                ed.on('init', function () {
-                    // get the current content
-                    var cont = ed.getContent();
+    if(typeof(tinyMCE) !== 'undefined') {
+        var length = tinyMCE.editors.length;
+        for (var i=length; i>0; i--) {
+            tinyMCE.editors[i-1].remove();
+        };
+    }
+}
 
-                    // If its empty and we have a placeholder set the value
-                    if (cont.length === 0) {
-                        ed.setContent(placeholder);
-                        // Get updated content
-                        cont = placeholder;
-                    }
-                    // convert to plain text and compare strings
-                    is_default = (cont == placeholder);
+BY.addEditor = function(){
+    var textAreas = $("textarea");
+    for (var i = 0; i < textAreas.length; i++) {
+        //Check if element already has editor enabled
+        if (tinymce.get(textAreas[i].id)) {
+            //Remove existing editor
+            tinyMCE.execCommand("mceRemoveEditor", false, textAreas[i].id);
+        }
 
-                    // nothing to do
-                    if (!is_default) {
-                        return;
-                    }
-                }).on('keydown', function () {
-                    // replace the default content on focus if the same as original placeholder
-                    if (is_default) {
-                        ed.setContent('');
-                        is_default = false;
-                    }
-                }).on('blur', function () {
-                    if (ed.getContent().length === 0) {
-                        ed.setContent(placeholder);
-                    }
-                });
-            }
-            ed.on('init', function (evt) {
-                var toolbar = $(evt.target.editorContainer)
-                    .find('>.mce-container-body >.mce-toolbar-grp');
-                var editor = $(evt.target.editorContainer)
-                    .find('>.mce-container-body >.mce-edit-area');
+        //Add editor
+        tinyMCE.execCommand("mceAddEditor", false, textAreas[i].id);
+    }
+}
 
-                // switch the order of the elements
-                toolbar.detach().insertAfter(editor);
-            });
-            ed.on("keyup", function () {
-                var id = ed.id;
-                if ($.trim(ed.getContent({format: 'text'})).length) {
-                    $("#" + id).parents(".textarea-label").find(".btn").removeClass("disabled");
-                } else {
-                    $("#" + id).parents(".textarea-label").find(".btn").addClass("disabled");
+
+
+tinymce.init({
+    mode: "textareas",
+    theme: "modern",
+    skin: 'light',
+    statusbar: false,
+    menubar: false,
+    plugins: [
+        "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+        "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+        " emoticons textcolor paste autoresize "
+    ],
+    toolbar: "styleselect | bold italic | bullist numlist hr  | undo redo | link unlink emoticons image media  preview ",
+    setup : function(ed) {
+        var placeholder = $('#' + ed.id).attr('placeholder');
+        if (typeof placeholder !== 'undefined' && placeholder !== false) {
+            var is_default = false;
+            ed.on('init', function () {
+                // get the current content
+                var cont = ed.getContent();
+
+                // If its empty and we have a placeholder set the value
+                if (cont.length === 0) {
+                    ed.setContent(placeholder);
+                    // Get updated content
+                    cont = placeholder;
+                }
+                // convert to plain text and compare strings
+                is_default = (cont == placeholder);
+
+                // nothing to do
+                if (!is_default) {
+                    return;
+                }
+            }).on('keydown', function () {
+                // replace the default content on focus if the same as original placeholder
+                if (is_default) {
+                    ed.setContent('');
+                    is_default = false;
+                }
+            }).on('blur', function () {
+                if (ed.getContent().length === 0) {
+                    ed.setContent(placeholder);
                 }
             });
-
-            ed.on('blur', function(e) {
-                console.log('reset event', e);
-            });
-
-            ed.on('remove', function(e) {
-                console.log('remove event', e);
-            });
         }
-    });
-}
+        ed.on('init', function (evt) {
+            var toolbar = $(evt.target.editorContainer)
+                .find('>.mce-container-body >.mce-toolbar-grp');
+            var editor = $(evt.target.editorContainer)
+                .find('>.mce-container-body >.mce-edit-area');
+
+            // switch the order of the elements
+            toolbar.detach().insertAfter(editor);
+        });
+        ed.on("keyup", function () {
+            var id = ed.id;
+            if ($.trim(ed.getContent({format: 'text'})).length) {
+                $("#" + id).parents(".textarea-label").find(".btn").removeClass("disabled");
+            } else {
+                $("#" + id).parents(".textarea-label").find(".btn").addClass("disabled");
+            }
+        });
+
+        ed.on('blur', function(e) {
+            console.log('reset event', e);
+        });
+
+        ed.on('remove', function(e) {
+            console.log('remove event', e);
+        });
+    }
+});
+
 
 BY.selectedCategoryList = {};
 BY.selectCategory = function(selectedInput){

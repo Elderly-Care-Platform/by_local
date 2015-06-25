ec2f7ca5cdb3af806716bd12499517b189bf8252
Nitin Jain <nitin.j@beautifulyears.com>
2015-06-25 16:46:44 +0530

    changing detail response structure

--- src/main/java/com/beautifulyears/rest/response/DiscussDetailResponse.java
+++ src/main/java/com/beautifulyears/rest/response/DiscussDetailResponse.java
@@ -20,116 +20,17 @@ import com.beautifulyears.domain.User;
 
 public class DiscussDetailResponse implements IResponse {
 
-	private String id;
-	private String title;
-	private String articlePhotoFilename;
-	private String userId;
-	private String username;
-	private String discussType; // Q, P and A (Question, Post and Article)
-	private String text;
-	private int aggrReplyCount;
-	private Date createdAt = new Date();
-	private List<String> topicId;
-	private boolean isLikedByUser = false;
-	private int aggrLikeCount = 0;
 	private List<DiscussReply> replies = new ArrayList<DiscussReply>();
+	private DiscussResponse.DiscussEntity discuss;
 	
 	
 
-	public String getId() {
-		return id;
+	public DiscussResponse.DiscussEntity getDiscuss() {
+		return discuss;
 	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getArticlePhotoFilename() {
-		return articlePhotoFilename;
-	}
-
-	public void setArticlePhotoFilename(String articlePhotoFilename) {
-		this.articlePhotoFilename = articlePhotoFilename;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getDiscussType() {
-		return discussType;
-	}
-
-	public void setDiscussType(String discussType) {
-		this.discussType = discussType;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public int getAggrReplyCount() {
-		return aggrReplyCount;
-	}
-
-	public void setAggrReplyCount(int aggrReplyCount) {
-		this.aggrReplyCount = aggrReplyCount;
-	}
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public List<String> getTopicId() {
-		return topicId;
-	}
-
-	public void setTopicId(List<String> topicId) {
-		this.topicId = topicId;
-	}
-
-	public boolean isLikedByUser() {
-		return isLikedByUser;
-	}
-
-	public void setLikedByUser(boolean isLikedByUser) {
-		this.isLikedByUser = isLikedByUser;
-	}
-
-	public int getAggrLikeCount() {
-		return aggrLikeCount;
-	}
-
-	public void setAggrLikeCount(int aggrLikeCount) {
-		this.aggrLikeCount = aggrLikeCount;
+	public void setDiscuss(DiscussResponse.DiscussEntity discuss) {
+		this.discuss = discuss;
 	}
 
 	public List<DiscussReply> getReplies() {
@@ -147,23 +48,13 @@ public class DiscussDetailResponse implements IResponse {
 	}
 	
 	public void addDiscuss(Discuss discuss){
-		id = discuss.getId();
-		title = discuss.getTitle();
-		articlePhotoFilename = discuss.getArticlePhotoFilename();
-		userId = discuss.getUserId();
-		username = discuss.getUsername();
-		discussType = discuss.getDiscussType();
-		text = discuss.getText();
-		aggrReplyCount = discuss.getAggrReplyCount();
-		createdAt = discuss.getCreatedAt();
-		topicId = discuss.getTopicId();
+		DiscussResponse	discussResponse = new DiscussResponse();
+		this.setDiscuss(discussResponse.getDiscussEntity(discuss, null));
 	}
 	
 	public void addDiscuss(Discuss discuss,User user){
-		addDiscuss(discuss);
-		if (null != user && discuss.getLikedBy().contains(user.getId())) {
-			isLikedByUser = true;
-		}
+		DiscussResponse	discussResponse = new DiscussResponse();
+		this.setDiscuss(discussResponse.getDiscussEntity(discuss,user));
 	}
 
 	public void addReplies(List<DiscussReply> replies,User user) {

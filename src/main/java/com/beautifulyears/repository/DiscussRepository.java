29abe7e7706fe97f0821a5a306b13fab34413435
Nitin Jain <nitin.j@beautifulyears.com>
2015-07-08 17:41:20 +0530

    image hover for deletion

--- src/main/java/com/beautifulyears/repository/DiscussRepository.java
+++ src/main/java/com/beautifulyears/repository/DiscussRepository.java
@@ -1,15 +1,17 @@
 package com.beautifulyears.repository;
 
+import java.io.Serializable;
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.beautifulyears.domain.Discuss;
 import com.beautifulyears.repository.custom.DiscussRepositoryCustom;
 
 @Repository
-public interface DiscussRepository extends MongoRepository<Discuss, String>, DiscussRepositoryCustom {
+public interface DiscussRepository extends PagingAndSortingRepository<Discuss, Serializable>, DiscussRepositoryCustom {
+	
 
     public List<Discuss> findAll();
     

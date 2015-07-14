8b6f25b03858fff8d6e8533699ab55c63baa76c5
sanjukta <sanjuktas@beautifulyears.com>
2015-07-14 15:35:14 +0530

    Aparnas changes for responsive design and scroll to top fixes

--- src/main/webapp/app/shared/util/byUtil.js
+++ src/main/webapp/app/shared/util/byUtil.js
@@ -35,3 +35,36 @@ BY.byUtil.inValidateSession = function(){
         window.location = "#/users/aboutUs";
     }
 }
+
+$(window).scroll(function(){
+	var windowHeight = $(window).height()/2;
+
+	if($("body").scrollTop() > windowHeight){
+		$(".topScroll").show();
+	}else
+	{
+		$(".topScroll").hide();
+	}
+	
+	////////////////
+	
+	var footerv1Height = $(".footer-v1").height()+10;
+	$(".topScroll").css('bottom', footerv1Height +"px");
+});
+
+$(document).ready(function() {
+	$(".topScroll").click(function(){
+		$('body').animate({
+			scrollTop: 0
+		}, 800);
+	});
+	
+	//////////////
+	
+	
+	var containerWidth = $(".container").width();
+	var windowWIdth2 = $(window).width();
+	var searchright = (windowWIdth2 - containerWidth)/2;
+	$(".searchWrapper").css('right', searchright+"px");
+
+});

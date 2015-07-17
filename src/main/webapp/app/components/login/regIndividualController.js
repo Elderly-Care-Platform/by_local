e2054934d1caeead047faa39531d217c51aeea1d
Aparna <aparna.r@beautifulyears.com>
2015-07-17 14:19:35 +0530

    Individual profile done Adding review rate htmls

---
+++ src/main/webapp/app/components/login/regIndividualController.js
@@ -0,0 +1,235 @@
+byControllers.controller('regIndividualController', ['$scope', '$rootScope', '$http', '$location', '$routeParams', 'UserProfile', 'ServiceTypeList',
+    function ($scope, $rootScope, $http, $location, $routeParams, UserProfile, ServiceTypeList) {
+        $scope.userId = localStorage.getItem("USER_ID");
+        $scope.selectedServices = {};
+        $scope.profileImage = [];
+        $scope.galleryImages = [];
+        $scope.submitted = false;
+        $scope.minCategoryError = false;
+
+        $scope.addressCallback = function (response) {
+            console.log(response);
+            $('#addressLocality').blur();
+            $scope.address.city = "";
+            $scope.address.locality = response.name;
+            $scope.address.country = "";
+            $scope.address.zip = "";
+
+            for (var i = 0; i < response.address_components.length; i++) {
+                if (response.address_components[i].types.length > 0) {
+                    if (response.address_components[i].types[0] == "locality") {
+                        $scope.address.city += response.address_components[i].long_name;
+                    }
+
+                    else if (response.address_components[i].types[0].indexOf("administrative_area_level_3") != -1) {
+                        $scope.address.city = response.address_components[i].long_name;
+                    }
+                    else if (response.address_components[i].types[0] == "country") {
+                        //this is the object you are looking for
+                        $scope.address.country = response.address_components[i].long_name;
+                    }
+                    else if (response.address_components[i].types[0] == "postal_code") {
+                        //this is the object you are looking for
+                        $scope.address.zip = response.address_components[i].long_name;
+                    }
+                }
+
+            }
+            $scope.address.streetAddress = response.formatted_address;
+
+
+        }
+
+        //Request service type list
+        $scope.ServiceTypeList = ServiceTypeList.get({}, function () {
+            console.log($scope.ServiceTypeList);
+            var selectedServices = $scope.serviceProviderInfo.services;
+            if(selectedServices.length > 0){
+                angular.forEach($scope.ServiceTypeList, function(type, index){
+                    if(selectedServices.indexOf(type.id) > -1){
+                        type.selected = true;
+                        $scope.selectServiceType(type);
+                    }
+
+                    angular.forEach(type.children, function(subType, index){
+                        if(selectedServices.indexOf(subType.id) > -1){
+                            subType.selected = true;
+                            $scope.selectServiceType(subType);
+                        }
+                    });
+                });
+            }
+
+        })
+
+        //Select type of services provided by the institute
+        $scope.selectServiceType = function (elem) {
+            if (elem.selected) {
+                $scope.selectedServices[elem.id] = elem;
+            } else {
+                delete $scope.selectedServices[elem.id];
+
+                if (elem.parentId && $scope.selectedServices[elem.parentId]) {
+                    delete $scope.selectedServices[elem.parentId];
+                }
+            }
+        }
+
+        //Prefill form with previously selected data
+        $scope.extractData = function () {
+            $scope.basicProfileInfo = $scope.profile.basicProfileInfo;
+            $scope.serviceProviderInfo = $scope.profile.serviceProviderInfo;
+            $scope.individualInfo = $scope.profile.individualInfo;
+            $scope.address = $scope.basicProfileInfo.userAddress;
+            $('#homeVisit')[0].checked = $scope.serviceProviderInfo.homeVisits;
+
+            if ($scope.address.country === null) {
+                $scope.address.country = "India";
+            }       
+                     
+        }
+
+        if ($scope.$parent.profile) {
+            $scope.profile = $scope.$parent.profile;
+            $scope.extractData();
+        } else {
+            $scope.profile = UserProfile.get({userId: $scope.userId}, function (profile) {
+                $scope.profile = profile.data;
+                $scope.extractData();
+            });
+        }
+
+
+        //Get location details based on pin code
+        $scope.getLocationByPincode = function (element) {
+            var element = document.getElementById("zipcode");
+            $scope.address.city = "";
+            $scope.address.locality = "";
+            $scope.address.country = "";
+            $http.get("api/v1/location/getLocationByPincode?pincode=" + $scope.address.zip)
+                .success(function (response) {
+                    if (response) {
+                        $scope.address.city = response.districtname;
+                        $scope.address.locality = response.officename;
+                        $scope.address.streetAddress = response.officename + ", Distt: " + response.districtname + " , State: " + response.statename;
+                    }
+                });
+        }
+
+        $scope.options = {
+            country: "in",
+            resetOnFocusOut: false
+
+        };
+
+
+        $('input[type=checkbox][data-toggle^=toggle]').bootstrapToggle();  //Apply bootstrap toggle for house visit option
+
+        //$scope.newAddress = new addressFormat($scope.basicProfileInfo.userAddress.length);
+        //$scope.basicProfileInfo.userAddress.push($scope.newAddress);
+
+        function addressFormat(index) {
+            return {
+                "index": index, "city": "", "zip": "", "locality": "", "landmark": "", "address": ""
+            }
+        }
+
+        //Function to be used to add additional address
+        $scope.addNewAddress = function () {
+            //if($scope.basicProfileInfo.userAddress.length < BY.regConfig.maxUserAddress){
+            //    $scope.newAddress = new addressFormat($scope.basicProfileInfo.userAddress.length);
+            //    $scope.basicProfileInfo.userAddress.push($scope.newAddress);
+            //}
+
+        }
+
+
+        //Add secondary phone numbers
+        $scope.addPhoneNumber = function () {
+            //var number = {value:""};
+            if ($scope.basicProfileInfo.secondaryPhoneNos.length < BY.regConfig.maxSecondaryPhoneNos) {
+                $scope.basicProfileInfo.secondaryPhoneNos.push("");
+            }
+            else{
+            	$(".add-phone").hide();
+            }
+        }
+
+        //Add secondary email details
+        $scope.addEmail = function () {
+            //var email = {value:""};
+            if ($scope.basicProfileInfo.secondaryEmails.length < BY.regConfig.maxSecondaryEmailId) {
+                $scope.basicProfileInfo.secondaryEmails.push("");
+            }
+            else{
+            	$(".add-email").hide();
+            }
+        }
+
+        //Delete profile Image
+        $scope.deleteProfileImage = function () {
+            $scope.profileImage = [];
+            $scope.basicProfileInfo.profileImage = null;
+        }
+
+        //Delete gallery images
+        $scope.deleteGalleryImage = function (img) {
+            var imgIndex = $scope.galleryImages.indexOf(img);
+            if (imgIndex > -1) {
+                $scope.galleryImages.splice(imgIndex, 1);
+            }
+            imgIndex = $scope.basicProfileInfo.photoGalleryURLs.indexOf(img);
+            if (imgIndex > -1) {
+                $scope.basicProfileInfo.photoGalleryURLs.splice(imgIndex, 1);
+            }
+        }
+
+
+        //Get service list array out of selectedService object
+        $scope.getServiceList = function () {
+            for (key in $scope.selectedServices) {
+                if ($scope.selectedServices[key] && $scope.selectedServices[key].parentId) {
+                    $scope.selectedServices[$scope.selectedServices[key].parentId] = $scope.selectedServices[key];
+                }
+            }
+
+            var finalServiceList = $.map($scope.selectedServices, function (value, key) {
+                return key;
+            });
+
+            return finalServiceList;
+        }
+
+        //Post individual form
+        $scope.postUserProfile = function (isValidForm) {
+            $scope.submitted = true;
+            $scope.minCategoryError = false;
+            $scope.serviceProviderInfo.services = $scope.getServiceList();
+            $scope.serviceProviderInfo.homeVisits = $('#homeVisit')[0].checked;
+
+            $scope.basicProfileInfo.profileImage = $scope.profileImage.length > 0 ? $scope.profileImage[0] : $scope.basicProfileInfo.profileImage ;
+            $scope.basicProfileInfo.photoGalleryURLs = $scope.basicProfileInfo.photoGalleryURLs.concat($scope.galleryImages);
+
+            if ($scope.serviceProviderInfo.services.length === 0) {
+                $scope.minCategoryError = true;
+            }
+
+            if (isValidForm.$invalid || $scope.minCategoryError) {
+                window.scrollTo(0, 0);
+            } else {
+                var userProfile = new UserProfile();
+                angular.extend(userProfile, $scope.profile);
+                userProfile.$update({userId: $scope.userId}, function (profileOld) {
+                    console.log("success");
+                    $scope.submitted = false;
+                    $scope.$parent.exit();
+                }, function (err) {
+                    console.log(err);
+                    $scope.$parent.exit();
+                });
+            }
+
+        }
+
+
+    }]);
---
+++ src/main/webapp/app/components/profile/individualProfileController.js
@@ -0,0 +1,33 @@
+//DIscuss All
+byControllers.controller('IndividualProfileController', ['$scope', '$rootScope', '$location', '$route', '$routeParams',
+    'UserProfile', '$sce',
+    function ($scope, $rootScope, $location, $route, $routeParams, UserProfile, $sce) {
+        console.log($routeParams);
+        $("#preloader").show();
+        $scope.userId = $routeParams.profileId;
+        
+        
+
+        $scope.individualProfile = UserProfile.get({userId:$scope.userId}, function (profile) {
+                $scope.individualProfile = profile.data;
+                $("#preloader").hide();
+                
+                var sexValue = {0:'Ms.', 1:'Mr.'};
+                profile.data.individualInfo.sex =  sexValue[profile.data.individualInfo.sex];
+                //console.log(profile.data.individualInfo.sex);
+            },
+            function (error) {
+                console.log("individual profile error");
+            });
+
+
+        $scope.slideGallery = function(dir){
+            if(dir==="r"){
+                $('.by-gallery-container').addClass('by-gallery-animate-right');
+            }else{
+                $('.by-gallery-container').removeClass('by-gallery-animate-right');
+            }
+
+        }
+
+    }]);

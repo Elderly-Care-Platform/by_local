cdbeb9125d7027388ffb9e8a038911746b8f5d2f
sanjukta <sanjuktas@beautifulyears.com>
2015-07-22 17:03:10 +0530

    added review rate icon showing previously selected rating/review by user

--- src/main/webapp/app/components/profile/reviewRateController.js
+++ src/main/webapp/app/components/profile/reviewRateController.js
@@ -9,12 +9,24 @@ byControllers.controller('ReviewRateController', ['$scope', '$rootScope', '$loca
         $scope.selectedRating = null;
         $scope.reviewText = "";
         $scope.blankReviewRateError = false;
+        var postReview = new ReviewRateProfile();
 
         if($scope.$parent.isIndividualProfile){
             $scope.gender =  BY.config.profile.userGender[$scope.userProfile.individualInfo.sex];
         }
 
-        //$scope.username = BY.validateUserName(localStorage.getItem("USER_NAME"));
+        $scope.getReview = function(){
+            postReview.$get({associatedId:$scope.userProfile.id, userId:localStorage.getItem("USER_ID")}, function(response){
+                var response = response.data.replies[0],
+                    ratingPercentage = BY.byUtil.getAverageRating(response.userRatingPercentage);
+                $scope.reviewText = response.text;
+                $scope.selectRating(ratingPercentage);
+            }, function(error){
+
+            })
+        }
+
+        $scope.getReview();
 
         $scope.selectRating = function(value){
             $(".profileSelected").removeClass("profileSelected");
@@ -25,7 +37,7 @@ byControllers.controller('ReviewRateController', ['$scope', '$rootScope', '$loca
         $scope.postReview = function(){
             if(parseInt($scope.selectedRating) > 0 || $scope.reviewText.trim().length > 0){
                 var ratePercentage = (parseInt($scope.selectedRating)/(parseInt(BY.config.profile.rate.upperLimit) - parseInt(BY.config.profile.rate.lowerLimit)))*100;
-                var postReview = new ReviewRateProfile();
+
                 postReview.userRatingPercentage = ratePercentage;
                 postReview.text = $scope.reviewText;
                 $scope.blankReviewRateError = false;

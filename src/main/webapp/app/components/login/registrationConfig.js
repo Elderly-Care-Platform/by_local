8117b08e8629c28114f399496c7d535b5132a11f
sanjukta <sanjuktas@beautifulyears.com>
2015-07-27 13:14:45 +0530

    updating config for registration

--- src/main/webapp/app/components/login/registrationConfig.js
+++ src/main/webapp/app/components/login/registrationConfig.js
@@ -1,14 +1,7 @@
 var BY = BY || {};
-
-BY.config = BY.config || {};
-BY.config.registration = BY.config.registration || {};
-
 BY.regConfig = {
     "maxSecondaryPhoneNos":3,
     "maxSecondaryEmailId":2,
     "maxUserAddress":20
 
-};
-
-
-BY.config.registration.websitePattern = "/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/";
+}

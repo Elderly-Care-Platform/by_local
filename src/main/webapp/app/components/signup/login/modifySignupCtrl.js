5bd46599e5470779ddd22226f6caa68ca7cccd49
sanjukta <sanjuktas@beautifulyears.com>
2015-08-25 12:48:46 +0530

    modify user credential implemented

--- src/main/webapp/app/components/signup/login/modifySignupCtrl.js
+++ src/main/webapp/app/components/signup/login/modifySignupCtrl.js
@@ -1,8 +1,32 @@
 byControllers.controller('modifySignUpCtrl', ['$scope', '$rootScope', '$http',
     function ($scope, $rootScope, $http) {
-        var emailValidation = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        $scope.userName = "";
+        $scope.password = "";
 
         $scope.modifyUserCredential = function(){
+            if(!$scope.userName && !$scope.password){
+                $scope.signUpErorr = "Username/Password empty";
+            } else if($scope.password && $scope.password.trim().length < 6){
+                $scope.signUpErorr = "Password must be at least 6 character";
+            }else{
+                $scope.signUpErorr = "";
+            }
+
+
+
+            if($scope.signUpErorr===""){
+                var newUserCredential = {
+                    "id" : $scope.$parent.userId,
+                    "userName" : $scope.userName,
+                    "password" : $scope.password
+                }
+                $http.post(apiPrefix +'api/v1/userProfile/', newUserCredential)
+                    .success(function (response) {
+                        console.log(response);
+                    }).error(function (error) {
+                        console.log(error);
+                    });
+            }
 
         }
     }]

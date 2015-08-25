89da200ff569cf1e602a130c21fd0bc91229bc0e
sanjukta <sanjuktas@beautifulyears.com>
2015-08-25 16:54:52 +0530

    displaying promotions on home page

--- src/main/webapp/app/components/home/homePromoController.js
+++ src/main/webapp/app/components/home/homePromoController.js
@@ -1,19 +1,43 @@
 
 byControllers.controller('BYHomePromoController', ['$scope', '$rootScope', '$routeParams', '$timeout', '$location', 'DiscussPage', '$sce', '$window','FindServices',
     function ($scope, $rootScope, $routeParams, $timeout, $location, DiscussPage, $sce, $window,FindServices) {
-		
-	$scope.getPromoDate = function(){
-		 DiscussPage.get({discussType: 'P',isFeatured:false,p:0,s:$scope.contentSize,sort:"lastModifiedAt"},
-                 function(value){
-                     $scope.promo = value.data.content;
-                     $scope.promoPageInfo = BY.byUtil.getPageInfo(value.data);
-                     $scope.promoPageInfo.isQueryInProgress = false;
-                 },
-                 function(error){
-                     console.log("DiscussPage");
-                 });
-	};
-	$scope.getPromoDate();
-        
-    }]);
+        $scope.getPromoData = function(){
+             DiscussPage.get({discussType:'P',isPromotion:true,p:0,s:$scope.$parent.contentSize,sort:"lastModifiedAt"},
+                     function(value){
+                         $scope.promo = value.data.content;
+                         $scope.promoPageInfo = BY.byUtil.getPageInfo(value.data);
+                         $scope.promoPageInfo.isQueryInProgress = false;
+                     },
+                     function(error){
+                         console.log("DiscussPage");
+                     });
+        };
+        $scope.getPromoData();
+
+        $scope.showMore = function(discussType){
+            $location.path($location.$$path).search({type: "promo"});
+        };
+
+        $scope.loadMore = function($event){
+            if($scope.promoPageInfo && !$scope.promoPageInfo.lastPage && !$scope.promoPageInfo.isQueryInProgress ){
+                $scope.promoPageInfo.isQueryInProgress = true;
+                var p = $scope.promoPageInfo.number + 1,
+                    s = $scope.promoPageInfo.size;
+
+                DiscussPage.get({discussType: $scope.contentType, isPromotion:true, p:p, s:s, sort:"lastModifiedAt"},
+                    function(value){
+                        if(value.data.content.length > 0){
+                            $scope.promoPageInfo.isQueryInProgress = false;
+                            $scope.promo = $scope.posts.concat(value.data.content);
+                        }
+                        $scope.promoPageInfo = BY.byUtil.getPageInfo(value.data);
+                        $scope.promoPageInfo.isQueryInProgress = false;
+                    },
+                    function(error){
+                        console.log("DiscussPage");
+                    });
+            }
+        }
+    }
+]);
 

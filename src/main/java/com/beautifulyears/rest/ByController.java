e33ec687f54f375f8fd08f011e90dbba7b7752a4
Nitin Jain <nitin.j@beautifulyears.com>
2015-08-26 18:14:20 +0530

    adding languages

---
+++ src/main/java/com/beautifulyears/rest/ByController.java
@@ -0,0 +1,46 @@
+package com.beautifulyears.rest;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.beautifulyears.domain.Language;
+import com.beautifulyears.rest.response.BYGenericResponseHandler;
+
+/**
+ * The REST based service for managing "discuss"
+ * 
+ * @author jumpstart
+ *
+ */
+@Controller
+@RequestMapping(value = { "/by" })
+public class ByController {
+	private static final Logger logger = Logger
+			.getLogger(ByController.class);
+	private MongoTemplate mongoTemplate;
+
+	@Autowired
+	public ByController(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+		// this.topicRepository = topicRepository;
+	}
+
+
+	@RequestMapping(method = { RequestMethod.GET }, value = { "/getLanguages" }, produces = { "application/json" })
+	@ResponseBody
+	public Object getLanguages()
+			throws Exception {
+		logger.debug("getting languages");
+		List<Language> languages = null;
+		languages = mongoTemplate.findAll(Language.class);
+		return BYGenericResponseHandler.getResponse(languages);
+	}
+}
---
+++ src/main/java/com/beautifulyears/rest/tempAddLanguages.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.beautifulyears.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.beautifulyears.domain.Language;
+import com.beautifulyears.rest.response.BYGenericResponseHandler;
+
+/**
+ * @author Nitin
+ *
+ */
+@Controller
+@RequestMapping(value = { "/addLanguages" })
+public class tempAddLanguages {
+
+	private MongoTemplate mongoTemplate;
+
+	@Autowired
+	public tempAddLanguages(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, value = { "/" }, produces = { "application/json" })
+	@ResponseBody
+	public Object addIsPromotion() throws Exception {
+		List<String> lang = new ArrayList<String>();
+		lang.add("Assamese (Asamiya)");
+		lang.add("Bengali");
+		lang.add("Bodo");
+		lang.add("Dogri");
+		lang.add("Gujarati");
+		lang.add("Hindi");
+		lang.add("Kannada");
+		lang.add("Kashmiri");
+		lang.add("Konkani");
+		lang.add("Maithili");
+		lang.add("Malayalam");
+		lang.add("Manipuri");
+		lang.add("Marathi");
+		lang.add("Nepali");
+		lang.add("Odia");
+		lang.add("Punjabi");
+		lang.add("Sanskrit");
+		lang.add("Santali");
+		lang.add("Sindhi");
+		lang.add("Tamil");
+		lang.add("Telugu");
+		lang.add("Urdu");
+		for (String l : lang) {
+			Language language = new Language();
+			language.setName(l);
+			mongoTemplate.save(language);
+		}
+		
+		return BYGenericResponseHandler.getResponse(null);
+	}
+}

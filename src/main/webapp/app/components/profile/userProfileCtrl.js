6cdca246fe2ac76ef03029e3f5699faab53c15c3
Aparna <aparna.r@beautifulyears.com>
2015-08-29 14:54:26 +0530

    Profile changes
    Changed personal story title from individual info
    Added some space if the story is not thr in personal story profile
    Changed null to anonymous

--- src/main/webapp/app/components/profile/userProfileCtrl.js
+++ src/main/webapp/app/components/profile/userProfileCtrl.js
@@ -5,7 +5,7 @@ byControllers.controller('ProfileController', ['$scope', '$rootScope', '$locatio
         $scope.profileViews = {};
         $scope.profileType = $routeParams.profileType;
         $scope.profileId = $routeParams.profileId;
-        $scope.userName = $routeParams.userName ? $routeParams.userName : "";
+        $scope.userName = $routeParams.userName ? BY.validateUserName($routeParams.userName) : "";
         $scope.isIndividualProfile = false;
         $scope.isAllowedToReview = false;
         $scope.reviewContentType = BY.config.profile.userType[$scope.profileType].reviewContentType;

28532ac6b46e7ab6b0d1c1c1a4931c39483fb096
sanjukta <sanjuktas@beautifulyears.com>
2015-09-07 19:39:38 +0530

    clicking on different branches on housing profile left panel implemented

--- src/main/webapp/app/components/profile/housingProfileLeftCtrl.js
+++ src/main/webapp/app/components/profile/housingProfileLeftCtrl.js
@@ -3,17 +3,23 @@
  */
 byControllers.controller('housingProfileLeftCtrl', ['$scope', '$rootScope', '$routeParams','broadCastData','$http',
     function ($scope, $rootScope, $routeParams, broadCastData, $http) {
+        $scope.otherBranches = null;
+        $scope.relatedFacilities = null;
 
         $scope.$on('handleBroadcast', function() {
             $scope.facilityData = broadCastData.newData;
             $http.get('api/v1/housing/getRelated?id=' + $scope.facilityData.id).success(function(response){
-                $scope.relatedFacilities = response.data;
-                $scope.otherBranches = $scope.relatedFacilities[$scope.facilityData.primaryAddress.city];
+                if(Object.keys(response.data).length > 0){
+                    $scope.relatedFacilities = response.data;
+                    $scope.otherBranches = $scope.relatedFacilities[$scope.facilityData.primaryAddress.city];
+                    if($scope.otherBranches && $scope.otherBranches.length <= 1){
+                        $scope.otherBranches = null;
+                    }
 
-
-                console.log($scope.relatedFacilities);
-                console.log($scope.otherBranches);
-
+                    if(Object.keys($scope.relatedFacilities).length <=1){
+                        $scope.relatedFacilities = null;
+                    }
+                }
             }).error(function(errorResponse){
                 console.log(errorResponse);
             });

d0fc33539aaf752f33ad8dd36083a1c01d5bbbbe
sanjukta <sanjuktas@beautifulyears.com>
2015-09-29 13:02:15 +0530

    user profile detail address validation

--- src/main/webapp/app/components/profile/userProfileCtrl.js
+++ src/main/webapp/app/components/profile/userProfileCtrl.js
@@ -194,19 +194,19 @@ define(['byApp', 'byUtil', 'userTypeConfig'],
                 $scope.formattedAddress = address.streetAddress;
             }
 
-            if(address.locality && $scope.formattedAddress.indexOf(address.locality)===-1){
+            if(!$scope.formattedAddress || (address.locality && $scope.formattedAddress.indexOf(address.locality)===-1)){
                 $scope.formattedAddress = $scope.formattedAddress + ", " + address.locality;
             }
 
-            if(address.city && $scope.formattedAddress.indexOf(address.city)===-1){
+            if(!$scope.formattedAddress || (address.city && $scope.formattedAddress.indexOf(address.city)===-1)){
                 $scope.formattedAddress = $scope.formattedAddress + ", " + address.city;
             }
 
-            if(address.country && $scope.formattedAddress.indexOf(address.country)===-1){
+            if(!$scope.formattedAddress || (address.country && $scope.formattedAddress.indexOf(address.country)===-1)){
                 $scope.formattedAddress = $scope.formattedAddress + ", " + address.country;
             }
 
-            if(address.zip && $scope.formattedAddress.indexOf(address.zip)===-1){
+            if(!$scope.formattedAddress || (address.zip && $scope.formattedAddress.indexOf(address.zip)===-1)){
                 $scope.formattedAddress = $scope.formattedAddress + " - " + address.zip;
             }
         }

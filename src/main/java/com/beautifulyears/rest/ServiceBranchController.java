2a1aca81bcdde8443d129dd3c8c5ca3452a55e59
saurav_beautifulyears <saurav.s@beautifulyears.com>
2015-12-10 22:03:02 +0530

    service branch update

--- src/main/java/com/beautifulyears/rest/ServiceBranchController.java
+++ src/main/java/com/beautifulyears/rest/ServiceBranchController.java
@@ -2,17 +2,25 @@ package com.beautifulyears.rest;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.beautifulyears.domain.ServiceBranch;
 import com.beautifulyears.domain.User;
 import com.beautifulyears.repository.ServiceBranchRepository;
+import com.beautifulyears.rest.response.BYGenericResponseHandler;
 
 @Controller
+@RequestMapping("/serviceBranch")
 public class ServiceBranchController {
 	
 	private static ServiceBranchRepository staticServiceBranchRepository;
@@ -24,6 +32,39 @@ public class ServiceBranchController {
 		staticServiceBranchRepository = serviceBranchRepository;
 		staticMongoTemplate = mongoTemplate;
 	}
+	
+	@RequestMapping(method = { RequestMethod.GET }, value = { "/getRelated" }, produces = { "application/json" })
+	@ResponseBody
+	public Object getRelatedService(
+			@RequestParam(value = "id", required = true) String id) {
+		ServiceBranch serviceBranch = staticServiceBranchRepository.findById(id);
+		List<ServiceBranch> serviceBranches = staticServiceBranchRepository
+				.findByUserId(serviceBranch.getUserId());
+		Map<String, List<ServiceBranch>> serviceMap = new HashMap<String, List<ServiceBranch>>();
+		for (ServiceBranch serviceBranch2 : serviceBranches) {
+			if (null != serviceBranch2.getBasicBranchInfo().getPrimaryUserAddress()
+					&& serviceMap.get(serviceBranch2.getBasicBranchInfo().getPrimaryUserAddress()
+							.getCity()) != null) {
+				serviceMap.get(serviceBranch2.getBasicBranchInfo().getPrimaryUserAddress().getCity())
+						.add(serviceBranch2);
+			} else if (null != serviceBranch2.getBasicBranchInfo().getPrimaryUserAddress()) {
+				List<ServiceBranch> cityList = new ArrayList<ServiceBranch>();
+				cityList.add(serviceBranch2);
+				serviceMap.put(serviceBranch2.getBasicBranchInfo().getPrimaryUserAddress().getCity(),
+						cityList);
+			} else {
+				List<ServiceBranch> cityList = serviceMap.get(null);
+				if (cityList != null) {
+					cityList.add(serviceBranch2);
+				} else {
+					cityList = new ArrayList<ServiceBranch>();
+					cityList.add(serviceBranch2);
+				}
+				serviceMap.put(null, cityList);
+			}
+		}
+		return BYGenericResponseHandler.getResponse(serviceMap);
+	};
 	
 	public static List<ServiceBranch> addServiceBranches(
 			List<ServiceBranch> serviceBranches, User user) {

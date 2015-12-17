856f16d811750a5ed9890417fa9154792e2ff4b2
saurav_beautifulyears <saurav.s@beautifulyears.com>
2015-12-17 17:03:24 +0530

    mail module changes

--- src/main/webapp/app/shared/common/js/shareController.js
+++ src/main/webapp/app/shared/common/js/shareController.js
@@ -5,13 +5,45 @@ define(["byApp"], function(byApp) {
 
         $scope.pathName = location.pathname;
         
-        $scope.shares = {}
+        $scope.shares = {};
+        $scope.emailError = "";
+        
+        $scope.resetError = function () {
+            $scope.emailError = "";
+            $scope.shareForm.emails.$invalid = "";
+        }
         
         $scope.checkSession = localStorage.getItem("SessionId");
         
-        $scope.emailShare = function emailShare(data) {
+        $scope.validateEmails = function() {
+        	var emailIds;
+        	if($scope.shares.email != undefined){
+        		emailIds = $scope.shares.email.split(',');
+            	for (var i = 0; i < emailIds.length; i++) {
+            		var emailValidation = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                    if(!emailValidation.test(emailIds[i])){
+                    	$scope.emailError = emailIds[i] + ' does not appear to be a proper email!';
+                    	$(".by_btn_submit").prop("disabled", true);
+                    }else{
+                        $scope.emailError = "";
+                        $(".by_btn_submit").prop("disabled", false);
+                    }       
+            	}
+        	}else{
+        		emailIds = null;
+        		$scope.emailError = "";
+        	}
+        	
+        }
+        	
+        $scope.modalDismiss = function modalDismiss() {
+        	$scope.resetError();
+        }
+        
+        $scope.emailShare = function emailShare(isValidForm, data) {
         	
         	var senderName = null;
+        	
         	if(localStorage.getItem("SessionId")==""){
         		senderName = $scope.shares.guestName;
         	}else{
@@ -124,22 +156,24 @@ define(["byApp"], function(byApp) {
             }
             
             var emailList = $scope.shares.email;
-            
             var emailIds = [];
             
-            emailIds = emailList.split(",");
+            emailIds = emailList.split(",");               
             
         	$scope.emailParams = {};
         	$scope.emailParams.emailIds = emailIds;
         	$scope.emailParams.subject = storyTitle;
         	$scope.emailParams.body = $scope.htmlBody;
-
-        	$http.post($scope.pathName + 'api/v1/share/email', $scope.emailParams   
-    		).success(function (response, status, headers, config) {
-    			
-    		}).error(function (data, status, headers, config) {    
-    			console.log(status);
-    		});
+        		
+        	if( $scope.emailError == ""){
+        		$http.post($scope.pathName + 'api/v1/share/email', $scope.emailParams   
+        		).success(function (response, status, headers, config) {
+    			 
+        		}).error(function(errorRes) {    
+        			console.log(errorRes);
+        			$(".by_btn_submit").prop("disabled", true);
+        		});
+        	}
         	
         }
         

8b73bf5ec6bedab3a89876aba170579ebd5e937a
sanjukta <sanjuktas@beautifulyears.com>
2015-12-23 16:16:09 +0530

    bug fixes for registration

--- src/main/webapp/app/shared/editor/editorController.js
+++ src/main/webapp/app/shared/editor/editorController.js
@@ -155,15 +155,10 @@ define(['byApp', 'byUtil', 'byEditor', 'userValidation'], function(byApp, byUtil
                 $scope.errorMsg = "Please select title";
             } else if($scope.discuss.text.trim().length <= 0){
                 $scope.errorMsg = "Please add more details";
+            } else if($scope.userSessionType === null && (!$scope.userCredential.email || !BY.byUtil.validateEmailId($scope.userCredential.email))){
+                $scope.errorMsg = "Please enter valid Email Id";
             } else{
                 $scope.errorMsg = "";
-            }
-
-
-            if($scope.userSessionType === null){
-                if(!$scope.userCredential.email || !BY.byUtil.validateEmailId($scope.userCredential.email)){
-                    $scope.errorMsg = "Please enter valid Email Id";
-                }
             }
 
         };

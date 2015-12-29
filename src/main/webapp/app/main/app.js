9b03b6953217cb026f7c0bca6c0d9864eccb6012
sanjukta <sanjuktas@beautifulyears.com>
2015-12-29 13:13:07 +0530

    adding new lib for image zoom

--- src/main/webapp/app/main/app.js
+++ src/main/webapp/app/main/app.js
@@ -39,7 +39,6 @@ define([
 			//window.scrollTo(0, 0);
 			BY.byEditor.removeEditor();
 			$rootScope.$broadcast('currentLocation', $location.path());
-			$(".zoomContainer").remove();
 
 			//For any location other than search, wipe out the search term
 			if($location.path().indexOf('/search/') == -1)

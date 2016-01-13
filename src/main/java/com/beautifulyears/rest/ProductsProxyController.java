c3dceae26a72a0b5581013aa9d8345d3bc788336
Nitin Jain <nitin.j@beautifulyears.com>
2016-01-13 13:00:04 +0530

    sending session and user in products apis

--- src/main/java/com/beautifulyears/rest/ProductsProxyController.java
+++ src/main/java/com/beautifulyears/rest/ProductsProxyController.java
@@ -24,6 +24,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import com.beautifulyears.domain.Session;
+import com.beautifulyears.domain.User;
+import com.beautifulyears.util.Util;
+
 /**
  * @author Nitin
  *
@@ -33,189 +37,200 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/products")
 public class ProductsProxyController {
 
-    @RequestMapping("/**")
-    @ResponseBody
-    public String mirrorProductsRest(@RequestBody String body,
-            HttpMethod method, HttpServletRequest request,
-            HttpServletResponse response) throws URISyntaxException {
-        String server = System.getProperty("productServerHost");
-        int port = Integer
-                .parseInt(System.getProperty("productServerPort"));
-        RestTemplate restTemplate = new RestTemplate();
-        if (null != request.getRequestURI()
-                && request.getRequestURI().indexOf("/products") > -1) {
-            String[] path = request.getRequestURI().split("/products");
-            try {
-                path[1] = URLDecoder.decode(path[1], "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                path[1] = path[1];
-            }
-            URI uri = new URI("http", null, server, port, path[1],
-                    request.getQueryString(), null);
+	@RequestMapping("/**")
+	@ResponseBody
+	public String mirrorProductsRest(@RequestBody String body,
+			HttpMethod method, HttpServletRequest request,
+			HttpServletResponse response) throws URISyntaxException {
+		String server = System.getProperty("productServerHost");
+		int port = Integer.parseInt(System.getProperty("productServerPort"));
+		RestTemplate restTemplate = new RestTemplate();
+		if (null != request.getRequestURI()
+				&& request.getRequestURI().indexOf("/products") > -1) {
+			String[] path = request.getRequestURI().split("/products");
+			try {
+				path[1] = URLDecoder.decode(path[1], "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				path[1] = path[1];
+			}
+			URI uri = new URI("http", null, server, port, path[1],
+					request.getQueryString(), null);
 
-            HttpHeaders headers = copyHeader(request, new HttpHeaders());
-            HttpEntity<String> entity = new HttpEntity<String>(body, headers);
+			HttpHeaders headers = copyHeader(request, new HttpHeaders());
+			HttpEntity<String> entity = new HttpEntity<String>(body, headers);
 
-            ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
-                    method, entity, String.class);
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
+					method, entity, String.class);
 
-            return responseEntity.getBody();
-        } else {
-            return null;
-        }
+			return responseEntity.getBody();
+		} else {
+			return null;
+		}
 
-    }
-    
-    @RequestMapping(value = { "/**" }, method = { RequestMethod.DELETE })
-    @ResponseBody
-    public String mirrorDELETEProductsRest(HttpMethod method, HttpServletRequest request,
-            HttpServletResponse response) throws URISyntaxException {
-        String server = System.getProperty("productServerHost");
-        int port = Integer
-                .parseInt(System.getProperty("productServerPort"));
-        RestTemplate restTemplate = new RestTemplate();
-        if (null != request.getRequestURI()
-                && request.getRequestURI().indexOf("/products") > -1) {
-            String[] path = request.getRequestURI().split("/products");
-            try {
-                path[1] = URLDecoder.decode(path[1], "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                path[1] = path[1];
-            }
-            URI uri = new URI("http", null, server, port, path[1],
-                    request.getQueryString(), null);
+	}
 
-            HttpHeaders headers = copyHeader(request, new HttpHeaders());
-            HttpEntity<String> entity = new HttpEntity<String>(headers);
+	@RequestMapping(value = { "/**" }, method = { RequestMethod.DELETE })
+	@ResponseBody
+	public String mirrorDELETEProductsRest(HttpMethod method,
+			HttpServletRequest request, HttpServletResponse response)
+			throws URISyntaxException {
+		String server = System.getProperty("productServerHost");
+		int port = Integer.parseInt(System.getProperty("productServerPort"));
+		RestTemplate restTemplate = new RestTemplate();
+		if (null != request.getRequestURI()
+				&& request.getRequestURI().indexOf("/products") > -1) {
+			String[] path = request.getRequestURI().split("/products");
+			try {
+				path[1] = URLDecoder.decode(path[1], "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				path[1] = path[1];
+			}
+			URI uri = new URI("http", null, server, port, path[1],
+					request.getQueryString(), null);
 
-            ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
-                    method, entity, String.class);
+			HttpHeaders headers = copyHeader(request, new HttpHeaders());
+			HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-            return responseEntity.getBody();
-        } else {
-            return null;
-        }
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
+					method, entity, String.class);
 
-    }
-    
-    @RequestMapping(value = { "/**" }, method = { RequestMethod.PUT })
-    @ResponseBody
-    public String mirrorPUTProductsRest(@RequestBody String body,
-            HttpMethod method, HttpServletRequest request,
-            HttpServletResponse response) throws URISyntaxException {
-        String server = System.getProperty("productServerHost");
-        int port = Integer
-                .parseInt(System.getProperty("productServerPort"));
-        RestTemplate restTemplate = new RestTemplate();
-        if (null != request.getRequestURI()
-                && request.getRequestURI().indexOf("/products") > -1) {
-            String[] path = request.getRequestURI().split("/products");
-            try {
-                path[1] = URLDecoder.decode(path[1], "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                path[1] = path[1];
-            }
-            URI uri = new URI("http", null, server, port, path[1],
-                    request.getQueryString(), null);
+			return responseEntity.getBody();
+		} else {
+			return null;
+		}
 
-            HttpHeaders headers = copyHeader(request, new HttpHeaders());
-            HttpEntity<String> entity = new HttpEntity<String>(body, headers);
+	}
 
-            ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
-                    method, entity, String.class);
+	@RequestMapping(value = { "/**" }, method = { RequestMethod.PUT })
+	@ResponseBody
+	public String mirrorPUTProductsRest(@RequestBody String body,
+			HttpMethod method, HttpServletRequest request,
+			HttpServletResponse response) throws URISyntaxException {
+		String server = System.getProperty("productServerHost");
+		int port = Integer.parseInt(System.getProperty("productServerPort"));
+		RestTemplate restTemplate = new RestTemplate();
+		if (null != request.getRequestURI()
+				&& request.getRequestURI().indexOf("/products") > -1) {
+			String[] path = request.getRequestURI().split("/products");
+			try {
+				path[1] = URLDecoder.decode(path[1], "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				path[1] = path[1];
+			}
+			URI uri = new URI("http", null, server, port, path[1],
+					request.getQueryString(), null);
 
-            return responseEntity.getBody();
-        } else {
-            return null;
-        }
+			HttpHeaders headers = copyHeader(request, new HttpHeaders());
+			HttpEntity<String> entity = new HttpEntity<String>(body, headers);
 
-    }
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
+					method, entity, String.class);
 
-    @RequestMapping(value = { "/**" }, method = { RequestMethod.GET })
-    @ResponseBody
-    public String mirrorGETProductsRest(HttpMethod method,
-            HttpServletRequest request, HttpServletResponse response)
-            throws URISyntaxException {
-        String server = System.getProperty("productServerHost");
-        int port = Integer
-                .parseInt(System.getProperty("productServerPort"));
-        RestTemplate restTemplate = new RestTemplate();
+			return responseEntity.getBody();
+		} else {
+			return null;
+		}
 
-        if (null != request.getRequestURI()
-                && request.getRequestURI().indexOf("/products") > -1) {
-            String[] path = request.getRequestURI().split("/products", 2);
-            try {
-                path[1] = URLDecoder.decode(path[1], "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                path[1] = path[1];
-            }
-            URI uri = new URI("http", null, server, port, path[1],
-                    request.getQueryString(), null);
+	}
 
-            HttpHeaders headers = copyHeader(request, new HttpHeaders());
-            HttpEntity<String> entity = new HttpEntity<String>(headers);
+	@RequestMapping(value = { "/**" }, method = { RequestMethod.GET })
+	@ResponseBody
+	public String mirrorGETProductsRest(HttpMethod method,
+			HttpServletRequest request, HttpServletResponse response)
+			throws URISyntaxException {
+		String server = System.getProperty("productServerHost");
+		int port = Integer.parseInt(System.getProperty("productServerPort"));
+		RestTemplate restTemplate = new RestTemplate();
 
-            ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
-                    method, entity, String.class);
+		if (null != request.getRequestURI()
+				&& request.getRequestURI().indexOf("/products") > -1) {
+			String[] path = request.getRequestURI().split("/products", 2);
+			try {
+				path[1] = URLDecoder.decode(path[1], "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				path[1] = path[1];
+			}
+			URI uri = new URI("http", null, server, port, path[1],
+					request.getQueryString(), null);
 
-            return responseEntity.getBody();
-        } else {
-            return null;
-        }
+			HttpHeaders headers = copyHeader(request, new HttpHeaders());
+			HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-    }
+			ResponseEntity<String> responseEntity = restTemplate.exchange(uri,
+					method, entity, String.class);
 
-    @RequestMapping(value = { "/images/**" }, method = { RequestMethod.GET })
-    @ResponseBody
-    public HttpEntity<byte[]> mirrorGETProductsImage(HttpMethod method,
-            HttpServletRequest request, HttpServletResponse response)
-            throws URISyntaxException {
-        String server = System.getProperty("productServerHost");
-        int port = Integer
-                .parseInt(System.getProperty("productServerPort"));
-        RestTemplate restTemplate = new RestTemplate();
+			return responseEntity.getBody();
+		} else {
+			return null;
+		}
 
-        if (null != request.getRequestURI()
-                && request.getRequestURI().indexOf("/products/images") > -1) {
-            String[] path = request.getRequestURI()
-                    .split("/products/images", 2);
-            try {
-                path[1] = URLDecoder.decode(path[1], "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                path[1] = path[1];
-            }
-            URI uri = new URI("http", null, server, port, path[1],
-                    request.getQueryString(), null);
-            System.out.println(uri.toString());
-            HttpHeaders headers = copyHeader(request, new HttpHeaders());
-            HttpEntity<byte[]> entity = new HttpEntity<byte[]>(headers);
+	}
 
-            ResponseEntity<byte[]> responseEntity = restTemplate.exchange(uri,
-                    method, entity, byte[].class);
-            headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG);
-            headers.setContentLength(responseEntity.getBody().length);
+	@RequestMapping(value = { "/images/**" }, method = { RequestMethod.GET })
+	@ResponseBody
+	public HttpEntity<byte[]> mirrorGETProductsImage(HttpMethod method,
+			HttpServletRequest request, HttpServletResponse response)
+			throws URISyntaxException {
+		String server = System.getProperty("productServerHost");
+		int port = Integer.parseInt(System.getProperty("productServerPort"));
+		RestTemplate restTemplate = new RestTemplate();
 
-            return new HttpEntity<byte[]>(responseEntity.getBody(), headers);
+		if (null != request.getRequestURI()
+				&& request.getRequestURI().indexOf("/products/images") > -1) {
+			String[] path = request.getRequestURI()
+					.split("/products/images", 2);
+			try {
+				path[1] = URLDecoder.decode(path[1], "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				path[1] = path[1];
+			}
+			URI uri = new URI("http", null, server, port, path[1],
+					request.getQueryString(), null);
+			System.out.println(uri.toString());
+			HttpHeaders headers = copyHeader(request, new HttpHeaders());
+			HttpEntity<byte[]> entity = new HttpEntity<byte[]>(headers);
 
-        } else {
-            return null;
-        }
+			ResponseEntity<byte[]> responseEntity = restTemplate.exchange(uri,
+					method, entity, byte[].class);
+			headers = new HttpHeaders();
+			headers.setContentType(MediaType.IMAGE_JPEG);
+			headers.setContentLength(responseEntity.getBody().length);
 
-    }
+			return new HttpEntity<byte[]>(responseEntity.getBody(), headers);
 
-    private HttpHeaders copyHeader(HttpServletRequest request,
-            HttpHeaders header) {
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            Enumeration<String> headers = request.getHeaders(headerName);
-            while (headers.hasMoreElements()) {
-                String headerValue = headers.nextElement();
-                header.set(headerName, headerValue);
-            }
-        }
-        return header;
-    }
+		} else {
+			return null;
+		}
+
+	}
+
+	private HttpHeaders copyHeader(HttpServletRequest request,
+			HttpHeaders header) {
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			Enumeration<String> headers = request.getHeaders(headerName);
+			while (headers.hasMoreElements()) {
+				String headerValue = headers.nextElement();
+				header.set(headerName, headerValue);
+			}
+		}
+		if (null != Util.getSessionUser(request)) {
+			Session currentSession = (Session) request.getSession()
+					.getAttribute("session");
+			User currentUser = Util.getSessionUser(request);
+			header.set("userId", currentUser.getId());
+			header.set("sessionId", request.getHeader("sess"));
+			header.set("email", currentUser.getEmail());
+			header.set("userName", currentUser.getUserName());
+			if (null != currentSession) {
+				header.set("sessionType", Integer
+						.toUnsignedString(currentSession.getSessionType()));
+			}
+
+		}
+
+		return header;
+	}
 
 }

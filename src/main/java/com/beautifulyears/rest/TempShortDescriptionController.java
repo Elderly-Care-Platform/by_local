597e65093a3d164c828746e6d6746d510f4c1114
Nitin Jain <nitin.j@beautifulyears.com>
2016-02-01 15:30:13 +0530

    adding script to edit shortSynopsis

--- src/main/java/com/beautifulyears/rest/TempShortDescriptionController.java
+++ src/main/java/com/beautifulyears/rest/TempShortDescriptionController.java
@@ -1,11 +1,11 @@
 package com.beautifulyears.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
@@ -14,18 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.beautifulyears.domain.User;
+import com.beautifulyears.constants.DiscussConstants;
+import com.beautifulyears.domain.Discuss;
 import com.beautifulyears.util.Util;
 //import com.beautifulyears.domain.UserProfile;
 
 @Controller
-@RequestMapping("/changePWD")
-public class TempPassController {
+@RequestMapping("/addShortDesc")
+public class TempShortDescriptionController {
 
 	private MongoTemplate mongoTemplate;
 
 	@Autowired
-	public TempPassController(
+	public TempShortDescriptionController(
 			MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
 	}
@@ -33,15 +34,18 @@ public class TempPassController {
 	@RequestMapping(method = RequestMethod.GET, value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Object changePassword(HttpServletRequest req,
 			HttpServletResponse res) throws Exception {
-		List<User> users = new ArrayList<User>();
-		users = mongoTemplate.findAll(User.class);
-		for(User user : users){
-			if(!Util.isEmpty(user.getPassword())){
-				user.setPassword(Util.getEncodedPwd(user.getPassword()));
-				System.out.println(user.getId()+"-"+user.getPassword());
-				mongoTemplate.save(user);
+		List<Discuss> discussList = mongoTemplate.findAll(Discuss.class);
+		for (Discuss discuss : discussList) {
+			org.jsoup.nodes.Document doc = Jsoup.parse(discuss.getText());
+			String domText = doc.text();
+			if (domText.length() > DiscussConstants.DISCUSS_TRUNCATION_LENGTH) {
+				discuss.setShortSynopsis(Util.truncateText(domText));
+			}else{
+				discuss.setShortSynopsis(domText);
 			}
+			mongoTemplate.save(discuss);
 		}
+		
 		return null;
 	}
 }

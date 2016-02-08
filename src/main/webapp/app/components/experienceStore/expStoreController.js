727b6d3cf90399fdc14cedcf335a2009e834e79f
Aparna <aparna.r@beautifulyears.com>
2016-02-08 18:02:17 +0530

    Experience store changes

--- src/main/webapp/app/components/experienceStore/expStoreController.js
+++ src/main/webapp/app/components/experienceStore/expStoreController.js
@@ -3,7 +3,15 @@ define(['byApp', 'byUtil'], function(byApp, byUtil) {
         
        $scope.telNo = BY.config.constants.byContactNumber;
        window.scrollTo(0, 0);
-       
+
+       $scope.showVideo = function(){       
+        $("#by_expVideoFrame").attr("src", 'https://www.youtube.com/embed/z9LkUOGUyrA?rel=0&showinfo=0&autoplay=1');
+       	var frameHeight = $(".by_expVideoShow").outerHeight();
+    	   $("#by_expVideoFrame").attr("height", frameHeight);
+       	$(".by_expVideoShow").hide();
+       	$("#by_expVideoFrame").show();
+       };
+      
     }
 
     expStoreCtrl.$inject = ['$scope', '$rootScope', '$routeParams', '$timeout', '$location', '$sce'];

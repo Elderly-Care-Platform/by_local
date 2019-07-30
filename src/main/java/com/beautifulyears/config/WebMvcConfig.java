0100e30b3655bc416615ccd2ac1cbf6d5b3fc2c2
Khushboo H <khushboo.hathiwala@gmail.com>
2019-07-30 18:42:17 +0530

    Implemented CORS fix

--- src/main/java/com/beautifulyears/config/WebMvcConfig.java
+++ src/main/java/com/beautifulyears/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -39,5 +40,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		registry.addInterceptor(loggerInterceptor).addPathPatterns("/**");
 		registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
 		;
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
 	}
 }
